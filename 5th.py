fe9e3828bef2653e486ffae0eb825bac275d6005
Evgeniy <zheinirk38@gmail.com>
2022-12-09 01:11:36 +0800

    5th task complet

--- 5th.py
+++ 5th.py
@@ -6,5 +6,25 @@
 # 4*x**2 + 1*x + 4
 # Результирующий файл:
 # 6*x**2 + 5*x + 9
+from sympy import symbols
+from math import prod
 
+f1 = open('5_1.txt', "r")
+f2 = open('5_2.txt', "r")
+f3 = open('5_end.txt', 'w')
 
+mnog1 = f1.read()
+mnog2 = f2.read()
+# print(len(mnog1))
+a = 0
+b = 0
+c = 0
+
+a = int(mnog1[0]) + int(mnog2[0])
+b = int(mnog1[9]) + int(mnog2[9])
+c = int(mnog1[-1]) + int(mnog2[-1])
+koeff = {a, b, c}
+x = symbols('x')
+
+mnog3 = str(sum(map(prod, zip(koeff, [x**i for i in range(3)]))))
+f3.write(mnog3)
